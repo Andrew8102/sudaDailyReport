c1116e68a0807375516e3a00a719a1e3820a84c6
Andrew <xzy565143480@163.com>
2020-08-18 16:57:43 +0800

    0818aff页面更新后修改代码

--- daka.py
+++ daka.py
@@ -1,19 +1,22 @@
 # encoding=utf-8
 
 from selenium import webdriver
-from time import strftime,gmtime
+from time import strftime, gmtime
 import requests
 import time
 import json
 
 # 开启浏览器
+
+
 def openChrome():
     option = webdriver.ChromeOptions()
     option.add_argument('disable-infobars')
     driver = webdriver.Chrome(options=option)
     return driver
 
-def find(driver,str):
+
+def find(driver, str):
     try:
         driver.find_element_by_xpath(str)
     except:
@@ -22,11 +25,13 @@ def find(driver,str):
         return True
 
 # 流程
+
+
 def operate_dk(driver):
     # 打开配置文件
     try:
-        with open("config.json",encoding='utf-8') as f:
-            config=json.load(f)
+        with open("config.json", encoding='utf-8') as f:
+            config = json.load(f)
     except:
         print("配置文件错误！请检查配置文件是否与py文件放置于同一目录下，或配置文件是否出错！\n请注意'现人员位置'只可填入以下四项中的任意一项：'留校', '在苏州', '江苏省内其他地区', '在其他地区'")
         return -1
@@ -49,22 +54,25 @@ def operate_dk(driver):
             print('登录成功！')
             url = "http://aff.suda.edu.cn/_web/fusionportal/detail.jsp?_p=YXM9MSZwPTEmbT1OJg__&id=2749&entranceUrl=http%3A%2F%2Fdk.suda.edu.cn%2Fdefault%2Fwork%2Fsuda%2Fjkxxtb%2Fjkxxcj.jsp&appKey=com.sudytech.suda.xxhjsyglzx.jkxxcj."
             driver.get(url)
-            driver.find_element_by_xpath("//*[@class='action-btn action-do']").click()
+            driver.find_element_by_xpath(
+                "//*[@class='action-btn action-do']").click()
 
             # 切换到小框内
-            iframe1=driver.find_element_by_id("layui-layer-iframe1")
+            iframe1 = driver.find_element_by_id("layui-layer-iframe1")
             driver.switch_to.frame(iframe1)
-            flag=False
-            while(flag==False):
+            flag = False
+            while(flag == False):
                 time.sleep(1)
-                flag=find(driver, "//*[text()='"+config["学号"]+"']")
+                flag = find(driver, "//*[text()='"+config["学号"]+"']")
 
-            driver.find_element_by_xpath("//*[@id='checkbox_jkzk259']").click()
-            xrywz={'留校':'radio_xrywz5','在苏州':'radio_xrywz7','江苏省内其他地区':'radio_xrywz9','在其他地区':'radio_xrywz37'}
-            driver.find_element_by_xpath("//*[@id='"+xrywz[config["现人员位置"]]+"']").click()
+            driver.find_element_by_xpath("//*[@id='checkbox_jkzk33']").click()
+            xrywz = {'在校': 'radio_xrywz5', '在苏州': 'radio_xrywz7',
+                     '江苏省内其他地区': 'radio_xrywz9', '在其他地区': 'radio_xrywz23'}
+            driver.find_element_by_xpath(
+                "//*[@id='"+xrywz[config["现人员位置"]]+"']").click()
             elem = driver.find_element_by_id("input_jtdz")
             elem.send_keys(config["家庭地址"])
-            driver.find_element_by_xpath("//*[@id='radio_sfyxglz41']").click()
+            driver.find_element_by_xpath("//*[@id='radio_sfyxglz27']").click()
 
             # 提交
             driver.find_element_by_xpath("//*[@id='tpost']").click()
@@ -72,37 +80,51 @@ def operate_dk(driver):
             try:
                 driver.find_element_by_xpath("//*[text()='提交成功！']")
                 print('打卡成功！')
-                driver.find_element_by_xpath("//a[@class='layui-layer-btn0']").click()
+                driver.find_element_by_xpath(
+                    "//a[@class='layui-layer-btn0']").click()
                 ddpost("今天的打卡已经成功执行")
                 wxpost("主人，打卡成功啦~")
             except:
                 try:
-                    driver.find_element_by_xpath("//*[text()='当天已经提交过，是否继续提交？']")
+                    driver.find_element_by_xpath(
+                        "//*[text()='当天已经提交过，是否继续提交？']")
                     print("已打卡过！")
-                    driver.find_element_by_xpath("//a[@class='layui-layer-btn0']").click()
+                    driver.find_element_by_xpath(
+                        "//a[@class='layui-layer-btn0']").click()
                     ddpost("今天的打卡已经成功执行")
                     wxpost("主人，打卡GET啦~")
                 except:
                     print("打卡失败！请重新开始")
-                    driver.find_element_by_xpath("//a[@class='layui-layer-btn0']").click()
+                    driver.find_element_by_xpath(
+                        "//a[@class='layui-layer-btn0']").click()
                     ddpost("今天的打卡失败，请手动打卡")
-                    wxpost("今天的打卡失败，请手动打卡")            
+                    wxpost("今天的打卡失败，请手动打卡")
             time.sleep(3)
             print("即将退出程序...")
             driver.quit()
 
-#钉钉webhook机器人推送
-def ddpost(content):
-	timenow = strftime("%Y-%m-%d %H:%M:%S", gmtime())
-	url = "https://oapi.dingtalk.com/robot/send?access_token="+config['ddkey']
-	data = '{"msgtype":"markdown","markdown":{"title":"每日疫情打卡","text":"# 每日打卡提醒 \n >### 消息提示：'+ content +' \n >### 打卡时间：'+ timenow +'"}}'
-	headers = {'Content-Type':'application/json'}
-	byte_data = data.encode("utf-8")
-	rep = requests.post(url=url, data=byte_data, headers=headers)
+# 钉钉webhook机器人推送
 
-#微信server酱机器人推送
+def ddpost(content):
+    with open("config.json", encoding='utf-8') as f:
+        config = json.load(f)
+    timenow = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+    url = "https://oapi.dingtalk.com/robot/send?access_token="+config['ddkey']
+    data = '{"msgtype":"markdown","markdown":{"title":"每日疫情打卡","text":"# 每日打卡提醒 \n >### 消息提示：' + \
+        content + ' \n >### 打卡时间：' + timenow + '"}}'
+    headers = {'Content-Type': 'application/json'}
+    byte_data = data.encode("utf-8")
+    rep = requests.post(url=url, data=byte_data, headers=headers)
+    print("钉钉打卡成功")
+
+# 微信server酱机器人推送
+
 def wxpost(content):
-	driver.get("https://sc.ftqq.com/"+config['wxkey']+".send?text="+content)
+    with open("config.json", encoding='utf-8') as f:
+        config = json.load(f)
+    driver.get("https://sc.ftqq.com/"+config['wxkey']+".send?text="+content)
+    print("微信打卡成功")
+
 
 # 主函数
 if __name__ == '__main__':
