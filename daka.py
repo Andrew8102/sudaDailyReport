f8f8997dc8012f6073c0bd19a1727353a4cdb446
Andrew <xzy565143480@163.com>
2020-08-28 08:51:03 +0800

    因为学校时不时更新，所以程序也要日常更新

--- daka.py
+++ daka.py
@@ -25,6 +25,8 @@ def find(driver, str):
         return True
 
 # 流程
+
+
 def operate_dk(driver):
     # 打开配置文件
     try:
@@ -34,17 +36,32 @@ def operate_dk(driver):
         print("配置文件错误！请检查配置文件是否与py文件放置于同一目录下，或配置文件是否出错！\n请注意'现人员位置'只可填入以下四项中的任意一项：'留校', '在苏州', '江苏省内其他地区', '在其他地区'")
         return -1
     else:
-        url = "http://ids1.suda.edu.cn/amserver/UI/Login?goto=http%3a%2f%2fmyauth.suda.edu.cn%2fdefault.aspx%3fapp%3dsswsswzx2%26jumpto%3d&welcome=%e5%b8%88%e7%94%9f%e7%bd%91%e4%b8%8a%e4%ba%8b%e5%8a%a1%e4%b8%ad%e5%bf%83&linkName=%e5%a6%82%e6%9e%9c%e6%97%a0%e6%b3%95%e4%bd%bf%e7%94%a8%e7%bb%9f%e4%b8%80%e8%ba%ab%e4%bb%bd%e7%99%bb%e5%bd%95%ef%bc%8c%e8%af%b7%e7%82%b9%e5%87%bb%e8%bf%99%e9%87%8c&linkUrl=http://aff.suda.edu.cn/_web/ucenter/login.jsp&gx_charset=UTF-8"
+        # 老版本登陆
+        # url = "http://ids1.suda.edu.cn/amserver/UI/Login?goto=http%3a%2f%2fmyauth.suda.edu.cn%2fdefault.aspx%3fapp%3dsswsswzx2%26jumpto%3d&welcome=%e5%b8%88%e7%94%9f%e7%bd%91%e4%b8%8a%e4%ba%8b%e5%8a%a1%e4%b8%ad%e5%bf%83&linkName=%e5%a6%82%e6%9e%9c%e6%97%a0%e6%b3%95%e4%bd%bf%e7%94%a8%e7%bb%9f%e4%b8%80%e8%ba%ab%e4%bb%bd%e7%99%bb%e5%bd%95%ef%bc%8c%e8%af%b7%e7%82%b9%e5%87%bb%e8%bf%99%e9%87%8c&linkUrl=http://aff.suda.edu.cn/_web/ucenter/login.jsp&gx_charset=UTF-8"
+        # driver.get(url)
+        # # 找到输入框并输入查询内容
+        # elem = driver.find_element_by_id("IDToken1")
+        # elem.send_keys(config["学号"])
+        # elem = driver.find_element_by_id("IDToken9")
+        # elem.send_keys(config["密码"])
+        # # 提交表单
+        # driver.find_element_by_xpath("//*[@id='loginBtn']").click()
+        # try:
+        #     driver.find_element_by_xpath("//*[text()='首页']")
+
+        # 新版登陆
+        url = "https://auth.suda.edu.cn/cas/login?service=https%3A%2F%2Fauth.suda.edu.cn%2Fsso%2Flogin%3Fredirect_uri%3Dhttps%253A%252F%252Fauth.suda.edu.cn%252Fsso%252Flogin%26x_client%3Dcas"
         driver.get(url)
         # 找到输入框并输入查询内容
-        elem = driver.find_element_by_id("IDToken1")
+        elem = driver.find_element_by_id("username")
         elem.send_keys(config["学号"])
-        elem = driver.find_element_by_id("IDToken9")
+        elem = driver.find_element_by_id("password")
         elem.send_keys(config["密码"])
         # 提交表单
-        driver.find_element_by_xpath("//*[@id='loginBtn']").click()
+        driver.find_element_by_xpath("//*[@id='fm1']/div[7]").click()
         try:
-            driver.find_element_by_xpath("//*[text()='首页']")
+            driver.find_element_by_xpath("//*[text()='账户信息']")
+
         except:
             print('登陆失败！请检查学号密码是否正确')
             return -1
@@ -52,8 +69,13 @@ def operate_dk(driver):
             print('登录成功！')
             url = "http://aff.suda.edu.cn/_web/fusionportal/detail.jsp?_p=YXM9MSZwPTEmbT1OJg__&id=2749&entranceUrl=http%3A%2F%2Fdk.suda.edu.cn%2Fdefault%2Fwork%2Fsuda%2Fjkxxtb%2Fjkxxcj.jsp&appKey=com.sudytech.suda.xxhjsyglzx.jkxxcj."
             driver.get(url)
-            driver.find_element_by_xpath("//*[@class='action-btn action-do']").click()
-
+            # 这个页面必须点两次，不晓得为啥
+            time.sleep(1)
+            driver.find_element_by_xpath(
+                "/html/body/div/div[2]/div/section/div/div[2]/div[1]/a[1]").click()
+            time.sleep(1)
+            driver.find_element_by_xpath(
+                "//*[@class='action-btn action-do']").click()
             # 切换到小框内
             iframe1 = driver.find_element_by_id("layui-layer-iframe1")
             driver.switch_to.frame(iframe1)
@@ -64,7 +86,7 @@ def operate_dk(driver):
             elem = driver.find_element_by_id("input_swtw")
             elem.send_keys("36.8")
             elem = driver.find_element_by_id("input_xwtw")
-            elem.send_keys("36.8")    	
+            elem.send_keys("36.8")
             driver.find_element_by_xpath("//*[@id='checkbox_jkzk35']").click()
             xrywz = {'在校': 'radio_xrywz5', '在苏州': 'radio_xrywz7',
                      '江苏省内其他地区': 'radio_xrywz9', '在其他地区': 'radio_xrywz23'}
@@ -105,6 +127,7 @@ def operate_dk(driver):
 
 # 钉钉webhook机器人推送
 
+
 def ddpost(content):
     with open("config.json", encoding='utf-8') as f:
         config = json.load(f)
@@ -118,6 +141,7 @@ def ddpost(content):
     print("钉钉打卡成功")
 
 # 微信server酱机器人推送
+
 
 def wxpost(content):
     with open("config.json", encoding='utf-8') as f:
