de3dd27986067ca297330c466c46c6eed348868f
Andrew <xzy565143480@163.com>
2021-01-21 11:02:04 +0800

    忘了装上linux无界面打卡模式，装上

--- daka.py
+++ daka.py
@@ -17,8 +17,8 @@ def openChrome():
     options.add_argument('window-size=1600x900') # 指定浏览器分辨率
     options.add_argument('--disable-gpu') # 谷歌文档提到需要加上这个属性来规避bug
     options.add_argument('--hide-scrollbars') # 隐藏滚动条, 应对一些特殊页面
-    #options.add_argument('blink-settings=imagesEnabled=false') # 不加载图片, 提升速度
-    #options.add_argument('--headless') # 浏览器不提供可视化页面.linux下如果系统不支持可视化不加这条会启动失败
+    options.add_argument('blink-settings=imagesEnabled=false') # 不加载图片, 提升速度
+    options.add_argument('--headless') # 浏览器不提供可视化页面.linux下如果系统不支持可视化不加这条会启动失败
     options.add_argument('disable-infobars')
     driver = webdriver.Chrome(options=options,executable_path='./chromedriver')
     return driver
